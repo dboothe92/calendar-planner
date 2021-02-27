e0e171131397a234ed40eee6f1330ed77722466b
Dan B <dbraunby@hotmail.com>
2021-02-27 11:15:07 -0700

    hourly plans save to local storage and are loaded when page is refreshed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-let  todaysDate = (dayjs().format("dddd MM/DD/YYYY, hh:mm A"));
+let todaysDate = (dayjs().format("dddd MM/DD/YYYY, hh:mm A"));
 let hours = [];
+let storeText = [];
 
 //Set current day in header
 $("#currentDay").text(todaysDate);
@@ -16,17 +17,17 @@ for (i = 9; i < 18; i++) {
 function createTimeBlocks () {
     //Creates div to hold columns
     $(".container").append("<div class = 'row'></div>")
-
+    
     //Creates 3 columns in row div
     for (i = 0; i < hours.length; i++) {
+        let hourlyId = "text" + i
+        let btnId = "button" + i
         $(".row").append(
             "<div class = 'col-2 hour'>" + hours[i].shown + "</div>",
-            "<div class = 'col-8'><textarea class = 'time-block' id = "+ 'text' + i + "></textarea></div>",
-            "<div class = 'col-2'><button class = 'saveBtn' id =" + 'button' + i + "></button></div>"
-            );
-
-            $("#button" + i).on("click", saveText)
-           
+            "<div class = 'col-8'><textarea class = 'time-block' id = "+ hourlyId + "></textarea></div>",
+            "<div class = 'col-2'><button class = 'saveBtn' id = "+ btnId +"></button></div>"
+        );
+        
         //Checks time and marks second column with proper color for past/present/future 
         if (hours[i].hour < dayjs().hour()) {
             $(".time-block").addClass("past");
@@ -35,13 +36,31 @@ function createTimeBlocks () {
         } else {
             $(".time-block").last().addClass("future");
         } 
+
+        $("#" + btnId).on("click", function(event) {
+            event.preventDefault();
+            let currentIndex = hourlyId.split('t')[2];
+            let hourlyPlan = document.querySelector("#" + hourlyId).value;
+            storeText[currentIndex] = hourlyPlan;
+            localStorage.setItem("storedPlans", JSON.stringify(storeText));
+        });
     };
+    loadHours();
 };
 
-function saveText () {
-    for (i = 0; i < hours.length; i++) {
-        localStorage.setItem(("text" + i) , $("#text" + i).val());
-    }
-};
+function loadHours () {
+    savedHours = JSON.parse(localStorage.getItem("storedPlans"));
 
+    if(!savedHours) {
+        savedHours = [];
+    };
+
+    storeText = savedHours
+
+    for(i = 0; i < hours.length; i++) {
+        document.querySelector("#text" + i).value = storeText[i];
+    }; 
+};
 createTimeBlocks();
+
+
