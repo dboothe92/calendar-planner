aea41e2f4f4857c31ea09850c7e3e1a0683476ff
Dan B <dbraunby@hotmail.com>
2021-02-27 11:47:46 -0700

    icons and updating clock

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,6 @@
-let todaysDate = (dayjs().format("dddd MM/DD/YYYY, hh:mm A"));
 let hours = [];
 let storeText = [];
 
-//Set current day in header
-$("#currentDay").text(todaysDate);
 
 //Generates array to hold working hours (i starts at 9 and ends at 5 military to account for a 9-5 schedule.)
 for (i = 9; i < 18; i++) {
@@ -11,6 +8,13 @@ for (i = 9; i < 18; i++) {
                     shown: dayjs().set('hour', i).format("ha")
                     }
     hours.push(hoursObj);
+};
+
+//Set current day in header and updats the clock
+function updateTime() {
+    let todaysDate = (dayjs().format("dddd MM/DD/YYYY, hh:mm A"));
+    $("#currentDay").text(todaysDate);
+    setInterval(updateTime, 1000);
 };
 
 //Creates each hour block
@@ -25,7 +29,7 @@ function createTimeBlocks () {
         $(".row").append(
             "<div class = 'col-2 hour'>" + hours[i].shown + "</div>",
             "<div class = 'col-8'><textarea class = 'time-block' id = "+ hourlyId + "></textarea></div>",
-            "<div class = 'col-2'><button class = 'saveBtn' id = "+ btnId +"></button></div>"
+            "<div class = 'col-2'><button class = 'saveBtn' id = "+ btnId +"><span class='oi oi-task'></span></button></div>"
         );
         
         //Checks time and marks second column with proper color for past/present/future 
@@ -37,30 +41,38 @@ function createTimeBlocks () {
             $(".time-block").last().addClass("future");
         } 
 
+        //Adds clickability to each button 
         $("#" + btnId).on("click", function(event) {
             event.preventDefault();
             let currentIndex = hourlyId.split('t')[2];
+
+            //On click will add to storage array and add it to local storage
             let hourlyPlan = document.querySelector("#" + hourlyId).value;
             storeText[currentIndex] = hourlyPlan;
             localStorage.setItem("storedPlans", JSON.stringify(storeText));
         });
     };
+
     loadHours();
 };
 
+//function to load stored stuff from local Storage
 function loadHours () {
     savedHours = JSON.parse(localStorage.getItem("storedPlans"));
 
+    //if there is nothing in local storage setting a new empty array. 
     if(!savedHours) {
         savedHours = [];
-    };
-
+    }
     storeText = savedHours
 
+    //Loops through each row and adds text from storeText array.
     for(i = 0; i < hours.length; i++) {
         document.querySelector("#text" + i).value = storeText[i];
     }; 
 };
+
 createTimeBlocks();
+updateTime();
 
 
